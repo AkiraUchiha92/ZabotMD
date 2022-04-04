e1f6dbee74c50b2c8addbed879f17fa26676a9a8
Akira YT Channel <97806782+AkiraUchiha92@users.noreply.github.com>
2022-04-04 17:38:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,34 @@ global.prems = ['6281327327914'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  rey: 'https://server-api-rey.herokuapp.com',
   xteam: 'https://api.xteam.xyz',
+  dhnjing: 'https://dhnjing.xyz',
+  bx: 'https://bx-hunter.herokuapp.com',
+  neoxr: 'https://api.neoxr.eu.org',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  bsbt: 'https://rest-beni.herokuapp.com/',
+  botstyle: 'https://botstyle-api.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  hardianto: 'https://hardianto.xyz'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
   'https://api.xteam.xyz': 'MIMINETBOT'
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://pencarikode.xyz': 'pais',
+  'https://api.neoxr.eu.org': 'M1TjwTwAHZ',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+  'https://rest-beni.herokuapp.com/': 'benniismaelapikey',
+  'https://botstyle-api.herokuapp.com': 'ZqqAVN5n',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://hardianto.xyz': 'hardianto'
 }
 
 // Sticker WM
