ac4f7442e89ec7a02df20d4364a13a26365a09f2
Akira YT Channel <97806782+AkiraUchiha92@users.noreply.github.com>
2022-04-04 18:08:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'MIMINETBOT'
+  'https://api.xteam.xyz': 'MIMINETBOT',
   'https://server-api-rey.herokuapp.com': 'apirey',
   'https://pencarikode.xyz': 'pais',
   'https://api.neoxr.eu.org': 'M1TjwTwAHZ',
